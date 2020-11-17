29e7989b9750b084387a012cbebe2828254c6720
alberthruado <31713127+alberthruado@users.noreply.github.com>
2020-11-17 09:26:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS STYLE LINK -->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body> 
+
+    <form action="" method="post">
+    <div class="div-container">
+        <label class="label">Login</label>
+        <div class="login-form">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <br><br>
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <br><br>
+            <button type="submit" name="btnsubmit">Sign-in</button>
+            <p class="small">&nbsp;</p>
+            <a href="#"><p class="small">Forgot your password?</p></a>
+            <a href="main/index.php"><p class="small">Register Here!</p></a>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
